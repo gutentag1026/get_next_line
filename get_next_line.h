f35d04799ce24933c8a29586541540f88e0943a6
Yulan Huang <yhuang@e1z4r14p1.42.us.org>
2018-04-02 21:24:53 -0700

    km

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yhuang <yhuang@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/01 23:23:37 by yhuang            #+#    #+#             */
+/*   Updated: 2018/04/02 20:34:52 by yhuang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# define BUFF_SIZE 16
+# define FD_MAX 4864
+
+int				get_next_line(const int fd, char **line);
+
+#endif
